dc0ffd24bede1567bad83b667d4e1faaff7cf17b
Chris Hoorn <hoorncj@rose-hulman.edu>
2012-05-01 15:07:49 -0400

    Passes all 6 steps and both scenarios

---
+++ ChatBot-hoorncj/chatbot.rb
@@ -0,0 +1,6 @@
+STDOUT.sync = true
+puts "What is your name?"
+name = gets.chomp
+puts "Hello, "+ name + "!\n"
+puts "1. Global Thermonuclear War\n2. Risk\n3. Stratego\n4. Battleship\n5. Axis and Allies\n"
+
